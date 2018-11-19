a250feba9e043d1d2b2fd19c6727f49831b18f8a
Jan Batelka <janbatelka@aberystwyth.imp.fu-berlin.de>
2018-11-19 15:18:37 +0100

    commit needed to pull

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,13 @@ radius:u32,
 
 
 
+
 impl State for DrawGeometry {
     fn new() -> Result<DrawGeometry> {
         Ok(DrawGeometry{radius:1})
     }
 
+<<<<<<< HEAD
    fn update(&mut self, _window: &mut Window) -> Result<()> {
 	self.radius = self.radius+1;
 	if (self.radius > 300){
@@ -26,14 +28,22 @@ impl State for DrawGeometry {
 	
 	Ok(())
    }
+=======
+    fn update(&mut self, _window: &mut Window) -> Result<()> {
+        updateSimon();
+        updateJan();
+        updateRieke();
+        Ok(())
+    }
+>>>>>>> f91459e10aa8a7b58d87bb78ab0329054c744104
 
     fn draw(&mut self, window: &mut Window) -> Result<()> {
         window.clear(Color::WHITE)?;
-        window.draw(&Rectangle::new((100, 100), (32, 32)), Col(Color::BLUE));
+        window.draw_ex(&Rectangle::new((100, 100), (32, 32)), Col(Color::BLUE), Transform::rotate(45), 0);
         window.draw_ex(&Rectangle::new((400, 300), (32, 32)), Col(Color::BLUE), Transform::rotate(45), 10);
         window.draw(&Circle::new((400, 300), 1+self.radius), Col(Color::GREEN));
         window.draw_ex(
-            &Line::new((50, 80),(600, 450)).with_thickness(2.0),
+            &Line::new((50, 80), (600, 450)).with_thickness(2.0),
             Col(Color::RED),
             Transform::IDENTITY,
             5
@@ -44,15 +54,27 @@ impl State for DrawGeometry {
             Transform::rotate(45) * Transform::scale((0.5, 0.5)),
             0
         );
+
         Ok(())
     }
 }
 
 fn main() {
+<<<<<<< HEAD
     println!("Hello, world!");
 	//hallo git!
 	
 
     		run::<DrawGeometry>("Draw Geometry", Vector::new(800, 600), Settings::default());
 	
+=======
+    run::<DrawGeometry>("Draw Geometry", Vector::new(800, 600), Settings::default());
+>>>>>>> f91459e10aa8a7b58d87bb78ab0329054c744104
 }
+
+fn updateSimon() {}
+
+fn updateJan() {}
+
+fn updateRieke() {}
+
