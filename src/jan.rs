2cf80ee30c9fa5f6da4ae8f5322b1e2da429c475
JanBat <janbatelka@gmail.com>
2018-12-01 02:41:59 +0100

    erste schritte (udp)

--- src/jan.rs
+++ src/jan.rs
@@ -5,20 +5,30 @@ extern crate quicksilver;
 use quicksilver::{
     lifecycle::{Window}
 };
+use std::net::UdpSocket;
 
 use ::{Game};
 
+
 pub struct JanUpdateStruct {
-    pub test:i32,
+    pub test: i32,
+    socket:UdpSocket,
 }
 
 pub fn init() ->JanUpdateStruct{
-    JanUpdateStruct{test:0}
+    let mut bind_socket = UdpSocket::bind("127.0.0.1:34254");
+
+    JanUpdateStruct{
+        test:0,
+        socket : bind_socket.unwrap(),
+    }
+
 }
 
 
 pub fn update(_window: &mut Window, _game: &mut Game){
-
+    let mut buf = [0; 10];
+    let (amt, src) = _game._jan_update_struct.socket.recv_from(&mut buf);
 }
 
 pub fn draw(_window: &mut Window, _game: &mut Game) {
